e70113896397c7a374c584c3c1124dd995b08d27
Majesty17 <a-o@qq.com>
2017-07-25 01:46:33 +0800

    first commit

---
+++ cs_superclipboard/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace cs_superclipboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(577, 398);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyDown);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "内容";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 398);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ cs_superclipboard/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Media;
+
+namespace cs_superclipboard
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer player = new SoundPlayer();
+        List<string> all_data = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            player.SoundLocation = @"C:\Windows\Media\Windows Notify Messaging.wav";
+            player.Load();
+
+        }
+
+
+
+        //定时器
+        private void timer1_Tick(object sender, EventArgs e) {
+            IDataObject data = Clipboard.GetDataObject();
+            if (data.GetDataPresent(DataFormats.Text) == true) {
+                string text = (string)data.GetData(DataFormats.Text);
+                text = text.Trim();
+                if (text.Length > 0 && hasData(text)==false) {
+                    all_data.Add(text);
+                    showData();
+                    player.Play();
+                }
+            }
+        }
+
+        //显示数据
+        private void showData() {
+            listView1.Items.Clear();
+            for (int i = all_data.Count - 1; i >= 0; i--) {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = all_data[i];
+                listView1.Items.Add(lvi);
+            }
+            columnHeader1.Width = 540;
+
+        }
+
+        //是否存在
+        private bool hasData(string text)
+        {
+            for (int i = 0; i < all_data.Count; i++) {
+                if (all_data[i].Equals(text))
+                    return true;
+            }
+            return false;
+        }
+        
+        //删除
+        private void listView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            string text = (string)Clipboard.GetDataObject().GetData(DataFormats.Text);
+            if (e.KeyData == Keys.Delete) {
+                foreach (ListViewItem it in listView1.SelectedItems) {
+                    all_data.Remove(it.Text);
+                    if (text.Equals(it.Text) == true) {
+                        Clipboard.Clear();
+                    }
+                }
+                showData();
+            }
+        }
+    }
+}
