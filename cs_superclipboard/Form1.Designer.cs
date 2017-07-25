11a91356b0f0c92b4def74ccfb9f83014b962f51
majesty17 <a-o@qq.com>
2017-07-25 13:09:00 +0800

    almost useful

--- cs_superclipboard/Form1.Designer.cs
+++ cs_superclipboard/Form1.Designer.cs
@@ -29,9 +29,19 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.清除所有ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.在最前ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
@@ -40,34 +50,94 @@
             // 
             // listView1
             // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1});
+            this.columnHeader1,
+            this.columnHeader2});
             this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView1.FullRowSelect = true;
             this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(577, 398);
+            this.listView1.Size = new System.Drawing.Size(586, 276);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
             this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyDown);
             // 
             // columnHeader1
             // 
             this.columnHeader1.Text = "内容";
+            this.columnHeader1.Width = 500;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.清除所有ToolStripMenuItem,
+            this.在最前ToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.帮助ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 98);
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.帮助ToolStripMenuItem.Text = "帮助...";
+            this.帮助ToolStripMenuItem.Click += new System.EventHandler(this.帮助ToolStripMenuItem_Click);
+            // 
+            // 清除所有ToolStripMenuItem
+            // 
+            this.清除所有ToolStripMenuItem.Name = "清除所有ToolStripMenuItem";
+            this.清除所有ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.清除所有ToolStripMenuItem.Text = "清除所有";
+            this.清除所有ToolStripMenuItem.Click += new System.EventHandler(this.清除所有ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // 在最前ToolStripMenuItem
+            // 
+            this.在最前ToolStripMenuItem.CheckOnClick = true;
+            this.在最前ToolStripMenuItem.Name = "在最前ToolStripMenuItem";
+            this.在最前ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.在最前ToolStripMenuItem.Text = "在最前";
+            this.在最前ToolStripMenuItem.Click += new System.EventHandler(this.在最前ToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 398);
+            this.ClientSize = new System.Drawing.Size(586, 276);
             this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
-            this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -77,6 +147,14 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 清除所有ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem 在最前ToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
     }
 }
 
--- cs_superclipboard/Form1.cs
+++ cs_superclipboard/Form1.cs
@@ -9,6 +9,8 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Media;
 using System.Collections;
+using System.Resources;
+using System.Reflection;
 
 namespace cs_superclipboard
 {
@@ -16,6 +18,13 @@ namespace cs_superclipboard
     {
         SoundPlayer player = new SoundPlayer();
         List<string> all_data = new List<string>();
+        Help h = new Help();
+
+
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(
+            byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);  
+
 
         public Form1()
         {
@@ -24,14 +33,17 @@ namespace cs_superclipboard
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.Text = "超级剪切板 v0.1";
+
             timer1.Start();
             player.SoundLocation = @"C:\Windows\Media\Windows Notify Messaging.wav";
             player.Load();
 
             Hotkey hotkey;
             hotkey = new Hotkey(this.Handle);
-            Hotkey1 = hotkey.RegisterHotkey(System.Windows.Forms.Keys.F11, Hotkey.KeyFlags.MOD_CONTROL);   //定义快键(Ctrl + F2)
+            Hotkey1 = hotkey.RegisterHotkey(System.Windows.Forms.Keys.V, Hotkey.KeyFlags.MOD_WIN);   //定义快键(Ctrl + F2)
             hotkey.OnHotkey += new HotkeyEventHandler(OnHotkey);
+
 
         }
 
@@ -43,10 +55,23 @@ namespace cs_superclipboard
             if (data.GetDataPresent(DataFormats.Text) == true) {
                 string text = (string)data.GetData(DataFormats.Text);
                 text = text.Trim();
-                if (text.Length > 0 && hasData(text)==false) {
-                    all_data.Add(text);
-                    showData();
-                    player.Play();
+                //如果剪切板内容不为空，则进一步判断
+                if (text.Length > 0){
+                    //如果不在，则直接插入
+                    if (hasData(text) == false){
+                        //all_data.Add(text); 插入到第一，而不是追加
+                        all_data.Insert(0, text);
+                        showData();
+                        player.Play();
+                    }
+                    //如果在，并且不在第一位，则移动到第一位
+                    if (hasData(text) == true && all_data[0].Equals(text) == false) {
+                        int ind = all_data.IndexOf(text);
+                        string f_text = all_data[0];
+                        all_data[0] = text;
+                        all_data[ind] = f_text;
+                        showData();
+                    }
                 }
             }
         }
@@ -58,14 +83,17 @@ namespace cs_superclipboard
             {
                 ListViewItem lvi = new ListViewItem();
                 lvi.Text = all_data[i];
+                lvi.SubItems.Add("" + (i + 1));
                 listView1.Items.Add(lvi);
             }
-            columnHeader1.Width = 540;
+            columnHeader1.Width = 500;
 
             //
-            //头条插入当前剪切板
-            if (all_data.Count > 0)
+            //头条插入当前剪切板&加黑
+            if (all_data.Count > 0) {
+                (listView1.Items[0]).ForeColor = Color.Red;
                 Clipboard.SetData("Text", all_data[0]);
+            }
             else
                 Clipboard.Clear();
         }
@@ -92,6 +120,71 @@ namespace cs_superclipboard
                     }
                 }
                 showData();
+            }
+            if (e.KeyData == Keys.OemMinus) {
+                if (all_data.Count > 1) {
+                    string f_data = all_data[0];
+                    all_data.RemoveAt(0);
+                    all_data.Add(f_data);
+                    showData();
+
+                }
+            }
+            if (e.KeyData == Keys.Oemplus)
+            {
+                if (all_data.Count > 1)
+                {
+                    string f_data = all_data[all_data.Count-1];
+                    all_data.RemoveAt(all_data.Count - 1);
+                    all_data.Insert(0, f_data);
+                    showData();
+
+                }
+            }
+            if ((int)e.KeyData >= 49 && (int)e.KeyData <= 57) {
+                int ind = (int)e.KeyData - 48-1;
+                //MessageBox.Show("press:" + ind);
+                if (all_data.Count > ind && ind>0) {
+                    string a_data = all_data[ind];
+                    string f_data=all_data[0];
+                    all_data[0] = a_data;
+                    all_data[ind] = f_data;
+                    showData();
+                }
+            }
+        }
+        //双击提前
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count != 1 || listView1.Items[0].Selected==true)
+                return;
+            string s_text = listView1.SelectedItems[0].Text;
+            int ind = all_data.IndexOf(s_text);
+            string f_text = all_data[0];
+
+            all_data[0] = s_text;
+            all_data[ind] = f_text;
+            showData();
+        }
+
+        //双击通知栏图标
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                WindowState = FormWindowState.Normal;
+                this.Activate();
+                this.ShowInTaskbar = true;
+            }
+        }
+
+        //窗口大小变化
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                notifyIcon1.Visible = true;
             }
         }
 
@@ -168,14 +261,65 @@ namespace cs_superclipboard
                 return false;
             }
         }
-
+        //热键处理
         public void OnHotkey(int HotkeyID) 
         {
             if (HotkeyID == Hotkey1)
             {
-                
+                Input("hahaha");
+
             }
         }
+
+        public static bool Input(string str)
+        {
+            foreach (char ch in str)
+            {
+                //模拟单击键，可规避连续输入键值时遗漏或延长Thread.Sleep(50);  
+                //keybd_event(0x1, 0, 1, 0);
+                keybd_event(0x42, 0, 1, 0);
+            }
+            return false;
+        } 
+
+
+        //帮助
+        private void 帮助ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            h.Show();
+        }
+        //退出
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //清除所有
+        private void 清除所有ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            all_data.Clear();
+            Clipboard.Clear();
+            showData();
+        }
+
+        //是否最前
+        private void 在最前ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem mt = (ToolStripMenuItem)sender;
+            if (mt.Checked==false)
+            {
+                this.TopMost = false;
+                this.Opacity = 1.0;
+            }
+            else {
+                this.TopMost = true;
+                this.Opacity = 0.650;
+            }
+        }
+
+
+
+
+
 
     }
 }
